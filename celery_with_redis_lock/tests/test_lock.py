bf6ee9e4b081c15017e771e08494d785bdcc64c6
beetleman <mateusz.probachta@gmail.com>
2015-09-15 16:40:59 +0200

    add test for many locks

--- celery_with_redis_lock/tests/test_lock.py
+++ celery_with_redis_lock/tests/test_lock.py
@@ -10,9 +10,17 @@ def run_add_to_list(x):
     add_to_list.delay(x)
 
 
-def test_add_to_list(drop_list):
+def test_add_to_list_single_lock(drop_list):
     test_data = list(repeat(1, 10))
     with Pool(3) as p:
         p.map(run_add_to_list, test_data)
     time.sleep(5)
     assert [1] == sorted(map(int, get_list()))
+
+
+def test_add_to_list_many_locks(drop_list):
+    test_data = list(repeat(2, 10)) + list(repeat(3, 10)) + list(repeat(4, 10))
+    with Pool(3) as p:
+        p.map(run_add_to_list, test_data)
+    time.sleep(5)
+    assert [2, 3, 4] == sorted(map(int, get_list()))
